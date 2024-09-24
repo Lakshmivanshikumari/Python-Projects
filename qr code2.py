b7d41510637449e7b443d085fbdfed55db513f20
Lakshmivanshikumari <lakshmivknerkar996@gmail.com>
2024-09-24 13:49:04 +0530

    First commit

---
+++ qr code2.py
@@ -0,0 +1,10 @@
+import qrcode
+from PIL import Image
+import qrcode.constants
+
+qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_H,box_size=10,border=10,)
+
+qr.add_data("https://www.wscubetech.com/")
+qr.make(fit=True)
+img=qr.make_image(fill_color="white",back_color="pink")
+img.save("wscube_web.png")
